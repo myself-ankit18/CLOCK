9418b5fbe3259be29e327029e4fd4703849f5085
Ankit Kumar Dam <aparnadam1981@gmail.com>
2024-04-26 00:43:34 +0530

    U

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ setInterval(() => {
     hr = 30*hourHand + minuteHand/2 +secondHand/120;
     mr = 6*minuteHand + secondHand/10;
     sr = 6*secondHand;
+    tick.play();
 
     hours.style.transform = `rotate(${hr}deg)`;
     mins.style.transform = `rotate(${mr}deg)`;
     seconds.style.transform = `rotate(${sr}deg)`;
-    tick.play();
 
 }, 1000);
